5e835a4a1823e76b3f95a4876d8591f6459d2774
您的名字 <erik76821@gmail.com>
2026-03-13 14:40:40 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'job-inn-v1';
+const OFFLINE_URLS = [
+  '/Job-Inn/',
+  '/Job-Inn/index.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/Job-Inn/index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
